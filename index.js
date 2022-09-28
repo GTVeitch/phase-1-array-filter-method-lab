e08e5e720e69218e3f1ef7f91c4d15291abd94a2
Gavin Veitch <gavintveitch@gmail.com>
2022-09-28 13:23:19 -0600

    commit tree 7715bbd42f8e2f4e8d7e88e75a7640d6a038b45e

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solution here
+function findMatching(driversList, string){
+    return driversList.filter(function (drivers) {
+        return (drivers.toUpperCase() == string.toUpperCase())
+    })
+}
+
+function fuzzyMatch(driversList, string){
+    return driversList.filter(function (drivers) {
+        return (drivers[0] == string[0])
+    })
+}
+
+function matchName(driverList, string) {
+    return driverList.filter(function (drivers) {
+        return (drivers.name == string)
+    })
+}
